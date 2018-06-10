5dd123f8aa61df34b3b66cfc64f399caccaa09e1
sohe1l <soheilansari@icloud.com>
2018-06-10 14:16:23 -0700

    Added body and leg fragments

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -21,6 +21,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
 
 // This activity will display a custom Android image composed of three body parts: head, body, and legs
 public class AndroidMeActivity extends AppCompatActivity {
@@ -33,14 +34,25 @@ public class AndroidMeActivity extends AppCompatActivity {
 
         // Create a new head BodyPartFragment
         BodyPartFragment headFragment = new BodyPartFragment();
+        headFragment.setResImages(AndroidImageAssets.getHeads());
+        headFragment.setIndex(1);
 
-        // TODO (4) Set the list of image id's for the head fragment and set the position to the second image in the list
+        BodyPartFragment bodyFragment = new BodyPartFragment();
+        bodyFragment.setResImages(AndroidImageAssets.getBodies());
+        bodyFragment.setIndex(1);
+
+
+        BodyPartFragment legFragment = new BodyPartFragment();
+        legFragment.setResImages(AndroidImageAssets.getLegs());
+        legFragment.setIndex(1);
 
         // Add the fragment to its container using a FragmentManager and a Transaction
         FragmentManager fragmentManager = getSupportFragmentManager();
 
         fragmentManager.beginTransaction()
                 .add(R.id.head_container, headFragment)
+                .add(R.id.body_container, bodyFragment)
+                .add(R.id.leg_container, legFragment)
                 .commit();
 
         // TODO (5) Create and display the body and leg BodyPartFragments
--- app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -18,6 +18,7 @@ package com.example.android.android_me.ui;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,12 +27,21 @@ import android.widget.ImageView;
 import com.example.android.android_me.R;
 import com.example.android.android_me.data.AndroidImageAssets;
 
+import java.util.List;
+
 public class BodyPartFragment extends Fragment {
 
-    // TODO (1) Create a setter method and class variable to set and store of a list of image resources
+    private static final String LOG_TAG = "BODY_PART_FRAGMENT";
+    private List<Integer> resImages;
+    private int index;
 
-    // TODO (2) Create another setter method and variable to track and set the index of the list item to display
-        // ex. index = 0 is the first image id in the given list , index 1 is the second, and so on
+    public void setResImages(List<Integer> resImages) {
+        this.resImages = resImages;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the fragment
@@ -52,10 +62,13 @@ public class BodyPartFragment extends Fragment {
         ImageView imageView = (ImageView) rootView.findViewById(R.id.body_part_image_view);
 
         // Set the image to the first in our list of head images
-        imageView.setImageResource(AndroidImageAssets.getHeads().get(0));
 
-        // TODO (3) If a list of image ids exists, set the image resource to the correct item in that list
-        // Otherwise, create a Log statement that indicates that the list was not found
+        if(resImages != null){
+            imageView.setImageResource(resImages.get(index));
+
+        }else{
+            Log.v(LOG_TAG, "Null image list on body fragment.");
+        }
 
         // Return the rootView
         return rootView;
