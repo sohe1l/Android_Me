64d5ea4e2f5bd6316c8e135cf1602f04534481bf
sohe1l <soheilansari@icloud.com>
2018-06-11 11:36:16 -0700

    Linked main fragment activity to android me activity

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -16,8 +16,11 @@
 
 package com.example.android.android_me.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.android.android_me.R;
@@ -27,10 +30,35 @@ import com.example.android.android_me.R;
 public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageClickListener{
 
 
+    private int mHead, mBody, mLeg;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final Intent mainActivityIntent = new Intent(this, AndroidMeActivity.class);
+
+        // TODO (4) Get a reference to the "Next" button and launch the intent when this button is clicked
+        Button button = (Button) findViewById(R.id.button_next);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Bundle bundle = new Bundle();
+                bundle.putInt("head", mHead);
+                bundle.putInt("body", mBody);
+                bundle.putInt("leg", mLeg);
+
+                mainActivityIntent.putExtras(bundle);
+
+                startActivity(mainActivityIntent);
+            }
+        });
+
+
 
     }
 
@@ -40,10 +68,23 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
         Toast.makeText(this, "Position clicked = " + position, Toast.LENGTH_SHORT).show();
 
         // TODO (2) Based on where a user has clicked, store the selected list index for the head, body, and leg BodyPartFragments
+        int clickType = position / 12;
+        int bodyPartIndex = position - 12 * clickType;
+        switch (clickType){
+            case 0:
+                mHead = bodyPartIndex;
+                break;
+            case 1:
+                mBody = bodyPartIndex;
+                break;
+            case 2:
+                mLeg = bodyPartIndex;
+                break;
+        }
 
         // TODO (3) Put this information in a Bundle and attach it to an Intent that will launch an AndroidMeActivity
 
-        // TODO (4) Get a reference to the "Next" button and launch the intent when this button is clicked
+
 
     }
 
